91eed06c77f30c92323fcd762de4cd10ffa9bfc2
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-02 17:59:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <unistd.h>
 /**
- * main - print size of data
- *
- * Return: Always 0
+ * main - Entry point
+ *   
+ * Return: Always 1(Success) 
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
 	return (1);
 }
