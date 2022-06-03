738a4fab51f53743300ef9eada1aa514858949c2
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-02 17:32:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *
- * * main - program prints with function puts
- *
- * *
- *
- * *Return: Always 0
+ * main - main block
+ * Return: 0
  */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
