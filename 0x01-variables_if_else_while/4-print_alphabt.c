b4c17561b0118f9bb3d9018ea94ce9fed1fbf807
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-03 18:37:59 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,24 +4,24 @@
 
 #include <stdio.h>
 
-
-
 /**
- * main - a program that prints alphabets
+ * main - a program that prints alphabets except q and e
  *
  * Return: return 0
  */
-
 int main(void)
-
 {
 		char ch;
 
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-		putchar(ch);
+			if (ch == 'q' || ch == 'e')
+			{
+			}
+			else
+			{
+				putchar(ch);																 }
 		}
 		putchar('\n');
 		return (0);
-
 }
