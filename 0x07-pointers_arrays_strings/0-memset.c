51f1db01a2fc9eb8d1c57db4530a0d58b6f80512
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-20 14:50:54 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,17 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int j = 0
 	unsigned int i;
-
-	while (*(s + j) != '\0')
-	{
-		j++;
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
