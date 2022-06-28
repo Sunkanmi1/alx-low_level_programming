2b74f8c4be747970579dee624302e19db6494017
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-27 19:55:59 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * rgstostr - concatenates all the arguments of a program
+ * argstostr - concatenates all the arguments of a program
  * @ac: argument count
  * @av: argument vector
  * Return: pointer of an array of char
