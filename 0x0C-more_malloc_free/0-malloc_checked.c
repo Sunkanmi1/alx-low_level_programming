501dc6433616964cca3ef6f5060c18c3a0b73c18
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-30 13:25:14 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	int *m = malloc(b);
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	if (m == 0)
 		exit(98);
-	return (ptr);
+
+	return (m);
 }
