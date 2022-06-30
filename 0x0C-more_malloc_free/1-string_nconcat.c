247c6ec600c8d0aa3578bbf5ddec4ecbefc3942e
Sunkanmi1 <mustaphasunkanmi@gmail.com>
2022-06-30 13:53:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,36 +9,42 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char*s;
-	unsigned inti=0,j=0,len1=0,len2=0;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-	while(s1 && s1[len1])
-		        len1 ++;
+	if (s1 == NULL)
+		s1 = "";
 
-	while(s2 && s2[len2])
-		        len2 ++;
+	if (s2 == NULL)
+		s2 = "";
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < len1)
+	while (s1[size1] != '\0')
 	{
-		s[i] = s1[i];
-		i++;
+		size1++;
 	}
 
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
+	if (n > size2)
+		n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 
-	s[i] = '\0';
+	if (p == NULL)
+		return (0);
 
-	return (s);
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+	return (p);
 }
